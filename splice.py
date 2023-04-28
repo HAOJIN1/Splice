9e7a3d709c980223d944f6f6227f5837d21be466
jinhao <1565948255@qq.com>
2023-04-29 02:17:20 +0800

    A new version! This programming can get current dir all of images and splice them by default order.

--- splice.py
+++ splice.py
@@ -1,11 +1,26 @@
+# -*- coding: UTF-8 -*-
 import cv2
 import os
 import numpy as np
 
-def c(page_num):
-    n = page_num
-    imgList = [str(i+1).zfill(2)+".png" for i in range(n)]
-
+def load_img():
+    dict = os.getcwd()
+    files_list = os.listdir(dict)
+    img_list = []
+    for i in files_list:
+        if i[-3:] in ['png','jpg']:
+            img_list.append(i)
+    return img_list
+    
+def main():
+    load_img()
+    n = len(load_img())
+    # n = page_num
+    # if n < 10:
+    #     imgList = [r'0_Page_'+str(i+1)+".png" for i in range(n)]
+    # if n >= 10 and n < 100:
+    #     imgList = [r'0_Page_'+str(i+1).zfill(2)+".png" for i in range(n)]
+    imgList = load_img()
     img = [imgList[0]]*n
     for i in range(n):
         img[i] = cv2.imread(imgList[i])
@@ -19,5 +34,6 @@ def c(page_num):
     # cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    page_num = 10
-    c(page_num)
+    # conda activate cv
+    main()
+
